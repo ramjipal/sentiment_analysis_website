0bb2d88d426f90f1e2135d3f52edfa7c1c0b053f
ramjipal <92622199+ramjipal@users.noreply.github.com>
2024-03-25 18:48:17 +0530

    initial commit wiht only youtube sentiment analysis

--- sentiment_emotion_analysis/sentiment/views.py
+++ sentiment_emotion_analysis/sentiment/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
-from .forms import Sentiment_Typed_Tweet_analyse_form
 from .sentiment_analysis_code import sentiment_analysis_code
-from .forms import Sentiment_Imported_Tweet_analyse_form
-from .tweepy_sentiment import Import_tweet_sentiment
+from .youtube_scrapper import *
 
 
 def sentiment_analysis(request):
@@ -10,44 +8,31 @@ def sentiment_analysis(request):
 
 def sentiment_analysis_type(request):
     if request.method == 'POST':
-        form = Sentiment_Typed_Tweet_analyse_form(request.POST)
+        form = request.POST
         analyse = sentiment_analysis_code()
-        if form.is_valid():
-            tweet = form.cleaned_data['sentiment_typed_tweet']
-            sentiment = analyse.get_tweet_sentiment(tweet)
-            args = {'tweet':tweet, 'sentiment':sentiment}
-            return render(request, 'home/sentiment_type_result.html', args)
+        tweet = form['sentiment_typed_tweet']
+        sentiment = analyse.get_tweet_sentiment(tweet)
+        args = {'tweet':tweet, 'sentiment':sentiment}
+        return render(request, 'home/sentiment_type_result.html', args)
 
     else:
-        form = Sentiment_Typed_Tweet_analyse_form()
         return render(request, 'home/sentiment_type.html')
 
 def sentiment_analysis_import(request):
     if request.method == 'POST':
-        form = Sentiment_Imported_Tweet_analyse_form(request.POST)
-        tweet_text = Import_tweet_sentiment()
-        analyse = sentiment_analysis_code()
-
-        if form.is_valid():
-            handle = form.cleaned_data['sentiment_imported_tweet']
-
-            if handle[0]=='#':
-                list_of_tweets = tweet_text.get_hashtag(handle)
-                list_of_tweets_and_sentiments = []
-                for i in list_of_tweets:
-                    list_of_tweets_and_sentiments.append((i,analyse.get_tweet_sentiment(i)))
-                args = {'list_of_tweets_and_sentiments':list_of_tweets_and_sentiments, 'handle':handle}
-                return render(request, 'home/sentiment_import_result_hashtag.html', args)
-
-            list_of_tweets = tweet_text.get_tweets(handle)
-            list_of_tweets_and_sentiments = []
-            if handle[0]!='@':
-                handle = str('@'+handle)
-            for i in list_of_tweets:
-                list_of_tweets_and_sentiments.append((i,analyse.get_tweet_sentiment(i)))
-            args = {'list_of_tweets_and_sentiments':list_of_tweets_and_sentiments, 'handle':handle}
-            return render(request, 'home/sentiment_import_result.html', args)
+        form = request.POST
+        analyse = sentiment_analysis_code()  # a class for sentiment analysis
+        video_url = form['sentiment_imported_tweet']
+        id  = video_url[-11:]
+        list_of_tweets = get_tweets(id)
+        list_of_tweets_and_sentiments = []
+        print(list_of_tweets_and_sentiments)
+        for i in list_of_tweets:
+            list_of_tweets_and_sentiments.append((i,analyse.get_tweet_sentiment(i)))
+            
+        args = {'list_of_tweets_and_sentiments':list_of_tweets_and_sentiments, 'handle':id}
+        print(args)
+        return render(request, 'home/sentiment_import_result_hashtag.html', args)
 
     else:
-        form = Sentiment_Imported_Tweet_analyse_form()
         return render(request, 'home/sentiment_import.html')
